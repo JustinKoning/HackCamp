7eee58d419734b983d49736f7f02a3d005a41248
AdamWHY2K <68286215+AdamWHY2K@users.noreply.github.com>
2022-12-09 11:01:58 +0000

    Add login functionality

--- Models/User.php
+++ Models/User.php
@@ -16,4 +16,31 @@ class User {
         $stmt->bindParam(2, $password);
         $stmt->execute();
     }
+
+    public function logIn($email, $password){ //Logs user in
+        $hashed = '';
+        $UID = '';
+        $sqlQ = "SELECT password, UID FROM users WHERE email = ?";
+        $stmt = $this->_dbHandle->prepare($sqlQ);
+        $stmt->bindParam(1,$email);
+        $stmt->execute();
+        $results = $stmt->fetchAll();
+        foreach ($results as $result){
+            $hashed = $result[0];
+            $UID = $result[1];
+        }
+        if (password_verify($password, $hashed)){ //Verifies password user entered is same as the one in the database
+            $_SESSION["login"] = $email;
+            $_SESSION['UID'] = $UID;
+        }
+        else {
+            $_SESSION['loginError'] = 1; //Shows popup if email/password doesn't match
+        }
+    }
+
+    public function logOut(){ //Logs user out
+        unset($_SESSION["login"]);
+        unset($_SESSION['UID']);
+        session_destroy();
+    }
 }
