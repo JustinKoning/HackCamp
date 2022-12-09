c9cbe0c324bbae681f105d50211d17084d8916c8
JustinKoning <35217226+JustinKoning@users.noreply.github.com>
2022-12-09 12:06:15 +0000

    Fix for new login

--- vendors.php
+++ vendors.php
@@ -1,13 +1,13 @@
 <?php
 
+require_once("Models/User.php");
 require_once('Models/VendorDataSet.php');
 
+$user = new User();
 $view = new stdClass();
 $view->pageTitle = 'Homepage';
 
 $vendorDataSet = new VendorDataSet();
 $view->vendorDataSet = $vendorDataSet->fetchAssociatedVendors('1');
-
-require_once("logincontroller.php");
 
 require_once('Views/vendors.phtml');
