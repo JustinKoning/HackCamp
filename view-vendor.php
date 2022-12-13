373637952a96e5b55b0297a919a27483954ba126
JustinKoning <jkoning983@gmail.com>
2022-12-13 14:27:49 +0000

    Vendor Page + fixes
    Vendor Page SQL and HTML fixes
    View vendor functions and gets permissions from database

---
+++ view-vendor.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once("Models/User.php");
+require_once('Models/Vendor.php');
+
+$user = new User();
+$vendor = new Vendor($_GET['id']);
+$view = new stdClass();
+$view->pageTitle = 'View Vendor';
+$view->vendor = $vendor;
+
+if (isset($_POST["loginButton"])) { //Call logIn function when login button pressed using details user entered
+    $user->logIn($_POST["email"], $_POST["password"]);
+}
+
+if (isset($_POST["logoutButton"])){ //Call logOut function when logout button pressed
+    $user->logOut();
+}
+
+require_once('Views/view-vendor.phtml');
